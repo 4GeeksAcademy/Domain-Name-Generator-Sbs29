73d174fd312b56040ee37c193f12070b9b16f4ec
sbs29 <32124063+sbs29@users.noreply.github.com>
2024-05-17 18:53:03 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+
+  let adj = ["great", "big"];
+
+  let noun = ["jogger", "racoon"];
+
+  let ext = [".com", ".es", ".org"];
+
+  let result = "";
+
+  const generateDomain = () => {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < ext.length; l++) {
+            if (result == "") {
+              result = pronoun[i] + adj[j] + noun[k] + ext[l];
+            } else {
+              result =
+                result + "<br>" + (pronoun[i] + adj[j] + noun[k] + ext[l]);
+            }
+          }
+        }
+      }
+    }
+    return result;
+  };
+
+  const excuseElement = document.getElementById("domain");
+  excuseElement.innerHTML = generateDomain();
 };
